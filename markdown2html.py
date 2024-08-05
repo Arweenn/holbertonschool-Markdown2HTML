22f09b25bbd8229b8520d23dd4b255ffd63aa9d4
Arweenn <arwendumont25@outlook.fr>
2024-08-05 14:54:13 +0200

    Complete task 0

--- markdown2html.py
+++ markdown2html.py
@@ -11,7 +11,7 @@ import os
 
 def markdown2html(input_md, output_html):
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
@@ -19,5 +19,12 @@ def markdown2html(input_md, output_html):
         sys.stderr.write("Missing " + input_md + "\n")
         sys.exit(1)
 
-    else:
-        exit(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+
+
+markdown2html(sys.argv[1], sys.argv[2])
+exit(0)
