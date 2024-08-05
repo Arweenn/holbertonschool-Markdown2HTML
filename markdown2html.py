1223012d1c244f912b92da078ff32f94c7969532
Arweenn <arwendumont25@outlook.fr>
2024-08-05 16:09:08 +0200

    Task 1

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,32 @@ Script that takes 2 strings as arguments:
 
 import sys
 import os.path
+import re
+
+
+def convert_markdown_to_html(md_content):
+
+    lines = md_content.split('\n')
+    html_lines = []
+
+    heading_pattern = re.compile(r'^(#{1,6})\s*(.*)')
+
+    for line in lines:
+
+        heading_match = heading_pattern.match(line)
+
+        if heading_match:
+            heading_level = len(heading_match.group(1))
+            heading_text = heading_match.group(2)
+            html_lines.append(f'<h{heading_level}>{heading_text}</h{heading_level}>')
+
+        else:
+
+            if line.strip() == '':
+                html_lines.append(f'<p>{line}</p>')
+
+    html_text = '\n'.join(html_lines)
+    return html_text
 
 
 def main():
@@ -19,12 +45,16 @@ def main():
     if not os.path.isfile(mdfile):
         sys.stderr.write("Missing {}\n".format(mdfile))
         sys.exit(1)
-    else:
-        with open(mdfile, 'r') as f:
-            content = f.read()
-            with open(htmlfile, 'w') as file:
-                file.write(content)
-        sys.exit(0)
+
+    with open(mdfile, 'r') as f:
+        content = f.read()
+
+    html_content = convert_markdown_to_html(content)
+
+    with open(htmlfile, 'w') as file:
+        file.write(html_content)
+
+    sys.exit(0)
 
 
 if __name__ == "__main__":
