e64f1a9daaedc1773bed2fd17bb963a153e4afbe
Arweenn <arwendumont25@outlook.fr>
2024-08-05 14:55:26 +0200

    pep8

--- markdown2html.py
+++ markdown2html.py
@@ -19,12 +19,5 @@ def markdown2html(input_md, output_html):
         sys.stderr.write("Missing " + input_md + "\n")
         sys.exit(1)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        sys.exit(1)
-
-
 markdown2html(sys.argv[1], sys.argv[2])
 exit(0)
